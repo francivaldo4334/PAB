9391289b7187aa62d950ac11e9abead1898d31de
francivaldo4334 <francivaldodev@gmail.com>
2024-10-14 09:48:07 -0300

    :sparkles: feat: add actions map in domain

---
+++ build/domain/actions.js
@@ -0,0 +1,70 @@
+"use strict";
+function openMenuFile() {
+    console.log("TODO");
+}
+function openRecentProjects() {
+    console.log("TODO");
+}
+function openMenuNewElement() {
+    console.log("TODO");
+}
+function closeMenuNewElement() {
+    console.log("TODO");
+}
+function addNewElement(type) {
+    switch (type) {
+        case "OVAL":
+            console.log("TODO");
+            break;
+        case "RECT":
+            console.log("TODO");
+            break;
+        case "FRAME":
+            console.log("TODO");
+            break;
+        case "IMAGE":
+            console.log("TODO");
+            break;
+        default:
+            break;
+    }
+}
+function setMoveMode() {
+    console.log("TODO");
+}
+function setZoomMode() {
+    console.log("TODO");
+}
+function openMenuPlugins() {
+    console.log("TODO");
+}
+function openFolder(path) {
+    console.log("TODO");
+}
+function setEditMode(mode) {
+    switch (mode) {
+        case "DESIGN":
+            console.log("TODO");
+            break;
+        case "PROTOTYPE":
+            console.log("TODO");
+            break;
+        default:
+            break;
+    }
+}
+function addNewProp(type) {
+    switch (type) {
+        case "HTML":
+            console.log("TODO");
+            break;
+        case "CSS":
+            console.log("TODO");
+            break;
+        default:
+            break;
+    }
+}
+function removeProp(prop_id) {
+    console.log("TODO");
+}
