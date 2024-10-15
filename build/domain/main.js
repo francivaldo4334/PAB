d63cc02585feccda699ab25d1beeaec25dd05723
francivaldo4334 <francivaldodev@gmail.com>
2024-10-15 11:23:12 -0300

    :sparkles: feat: base

--- build/domain/main.js
+++ build/domain/main.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 function initNewProject() {
     current_project = base_json_template;
 }
@@ -6,12 +17,26 @@ function buildTag(component_json, mode_prod) {
     if (mode_prod === void 0) { mode_prod = false; }
     if (typeof component_json === "string")
         return component_json;
-    var is_tag_body = mode_prod && component_json.tag === "body";
-    var tag_name = (is_tag_body) ? "div" : component_json.tag;
-    var tag_props = (component_json.props) ? component_json.props.map(function (it) { return "".concat(it.name, "=\"").concat(it.value, "\""); }).join(" ") : "";
-    var tag_content = (component_json.content) ?
-        (!Array.isArray(component_json.content)) ? component_json.content
-            : component_json.content.map(function (it) { return buildTag(it); }).join("")
+    var comp = __assign({}, base_view_body);
+    var is_tag_comp = mode_prod && component_json.tag === "body";
+    if (is_tag_comp) {
+        var prop_style_1 = comp.props.find(function (it) { return it.name === "style"; });
+        comp.props = (component_json.props && component_json.props.length > 0) ? component_json.props.map(function (it) {
+            if (it.name === "style") {
+                return { "name": "style", "value": prop_style_1.value + it.value };
+            }
+            return it;
+        }) : comp.props;
+        comp.content = component_json.content;
+    }
+    else {
+        comp = component_json;
+    }
+    var tag_name = comp.tag;
+    var tag_props = (comp.props) ? comp.props.map(function (it) { return "".concat(it.name, "=\"").concat(it.value, "\""); }).join(" ") : "";
+    var tag_content = (comp.content) ?
+        (!Array.isArray(comp.content)) ? comp.content
+            : comp.content.map(function (it) { return buildTag(it); }).join("")
         : "";
     return "<".concat(tag_name, " ").concat(tag_props, ">").concat(tag_content, "</").concat(tag_name, ">");
 }
--- src/domain/main.ts
+++ src/domain/main.ts
@@ -5,12 +5,27 @@ function initNewProject() {
 function buildTag(component_json, mode_prod=false) {
   if (typeof component_json === "string")
     return component_json;
-  const is_tag_body = mode_prod && component_json.tag === "body";
-  const tag_name = (is_tag_body)? "div" : component_json.tag
-  const tag_props = (component_json.props)? component_json.props.map(it => `${it.name}="${it.value}"`).join(" ") : "";
-  const tag_content = (component_json.content)?
-      (!Array.isArray(component_json.content))? component_json.content
-      : component_json.content.map(it => buildTag(it)).join("")
+  let comp = {...base_view_body}
+  const is_tag_comp = mode_prod && component_json.tag === "body";
+  if (is_tag_comp){
+    const prop_style = comp.props.find(it => it.name === "style");
+
+    comp.props = (component_json.props && component_json.props.length > 0) ? component_json.props.map(it => {
+      if (it.name === "style") {
+        return {"name": "style","value": prop_style.value + it.value}
+      }
+      return it
+    }): comp.props;
+    comp.content = component_json.content;
+  }
+  else {
+    comp = component_json
+  }
+  const tag_name = comp.tag
+  const tag_props = (comp.props)? comp.props.map(it => `${it.name}="${it.value}"`).join(" ") : "";
+  const tag_content = (comp.content)?
+      (!Array.isArray(comp.content))? comp.content
+      : comp.content.map(it => buildTag(it)).join("")
     : "";
   return `<${tag_name} ${tag_props}>${tag_content}</${tag_name}>`
 }
