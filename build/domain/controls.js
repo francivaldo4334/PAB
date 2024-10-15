1232bfbc58ed56c743d8eb562cf6ee3f5b06e32f
francivaldo4334 <francivaldodev@gmail.com>
2024-10-15 16:43:00 -0300

    save

--- build/domain/controls.js
+++ build/domain/controls.js
@@ -11,12 +11,44 @@ var M_BUTTON_LEFT = false;
 var M_BUTTON_MIDDLE = false;
 var M_BUTTON_RIGHT = false;
 var SCROLL_STATE = "STOP";
+var SHIFT = false;
 var isScrolling;
+function setPositionDraw(x, y) {
+    var draw = document.getElementById("project_draw_position");
+    draw.style.transform = "translate(".concat(x, "px, ").concat(y, "px)");
+}
+function setPositionProject(x, y) {
+    current_project.position.x = x;
+    current_project.position.y = y;
+}
+function setScaleDraw(scale) {
+    var draw = document.getElementById("project_draw");
+    draw.style.scale = scale;
+}
+function setScaleProject(scale) {
+    current_project.zoom = scale;
+}
+function getScale() {
+    return current_project.zoom;
+}
+window.addEventListener("keydown", function (e) {
+    if (e.key == "Shift") {
+        SHIFT = true;
+    }
+});
+window.addEventListener("keyup", function (e) {
+    if (e.key == "Shift") {
+        SHIFT = false;
+    }
+});
 window.addEventListener("mousemove", function (e) {
     M_X = e.clientX;
     M_Y = e.clientY;
     M_DELTA_X = M_X - M_INIT_X;
     M_DELTA_Y = M_Y - M_INIT_Y;
+    if (EDIT_MODE === "MOVE" && M_BUTTON_LEFT) {
+        setPositionDraw(current_project.position.x + M_DELTA_X, current_project.position.y + M_DELTA_Y);
+    }
 });
 window.addEventListener("wheel", function (e) {
     if (e.deltaY < 0) {
@@ -29,6 +61,52 @@ window.addEventListener("wheel", function (e) {
     isScrolling = setTimeout(function () {
         SCROLL_STATE = "STOP";
     }, 300);
+    switch (EDIT_MODE) {
+        case "SELECTION":
+            var space = 20;
+            switch (SCROLL_STATE) {
+                case "UP":
+                    if (SHIFT) {
+                        setPositionDraw(current_project.position.x + space, current_project.position.y);
+                        setPositionProject(current_project.position.x + space, current_project.position.y);
+                    }
+                    else {
+                        setPositionDraw(current_project.position.x, current_project.position.y + space);
+                        setPositionProject(current_project.position.x, current_project.position.y + space);
+                    }
+                    break;
+                case "DOWN":
+                    if (SHIFT) {
+                        setPositionDraw(current_project.position.x - space, current_project.position.y);
+                        setPositionProject(current_project.position.x - space, current_project.position.y);
+                    }
+                    else {
+                        setPositionDraw(current_project.position.x, current_project.position.y - space);
+                        setPositionProject(current_project.position.x, current_project.position.y - space);
+                    }
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case "ZOOM":
+            var scale_space = 0.1;
+            switch (SCROLL_STATE) {
+                case "UP":
+                    setScaleDraw(current_project.zoom + scale_space);
+                    setScaleProject(current_project.zoom + scale_space);
+                    break;
+                case "DOWN":
+                    setScaleDraw(current_project.zoom - scale_space);
+                    setScaleProject(current_project.zoom - scale_space);
+                    break;
+                default:
+                    break;
+            }
+            break;
+        default:
+            break;
+    }
 });
 window.addEventListener("mouseup", function (e) {
     if (e.button === 0) {
@@ -40,15 +118,17 @@ window.addEventListener("mouseup", function (e) {
     else if (e.button === 2) {
         M_BUTTON_RIGHT = false;
     }
-    if (EDIT_MODE === "MOVE") {
-        current_project.position.x += M_DELTA_X;
-        current_project.position.y += M_DELTA_Y;
+    switch (EDIT_MODE) {
+        case "MOVE":
+            setPositionProject(current_project.position.x += M_DELTA_X, current_project.position.y += M_DELTA_Y);
+            break;
+        default:
+            break;
     }
     M_INIT_X = 0;
     M_INIT_Y = 0;
     M_DELTA_Y = 0;
     M_DELTA_Y = 0;
-    console.log("here");
 });
 window.addEventListener("mousedown", function (e) {
     if (e.button === 0) {
@@ -62,10 +142,4 @@ window.addEventListener("mousedown", function (e) {
     }
     M_INIT_X = e.clientX;
     M_INIT_Y = e.clientY;
-});
-window.addEventListener("mousemove", function (e) {
-    if (EDIT_MODE === "MOVE" && M_BUTTON_LEFT) {
-        var draw = document.getElementById("project_draw");
-        draw.style.transform = "translate(".concat(current_project.position.x + M_DELTA_X, "px, ").concat(current_project.position.y + M_DELTA_Y, "px)");
-    }
 });
--- src/domain/controls.ts
+++ src/domain/controls.ts
@@ -10,13 +10,48 @@ let M_BUTTON_LEFT = false;
 let M_BUTTON_MIDDLE = false;
 let M_BUTTON_RIGHT = false;
 let SCROLL_STATE = "STOP";
+let SHIFT = false;
 
 let isScrolling;
+function setPositionDraw(x,y) {
+  const draw = document.getElementById("project_draw_position");
+  draw.style.transform = `translate(${x}px, ${y}px)`;
+}
+function setPositionProject(x,y) {
+  current_project.position.x = x;
+  current_project.position.y = y;
+}
+function setScaleDraw(scale) {
+  const draw = document.getElementById("project_draw");
+  draw.style.scale = scale;
+}
+function setScaleProject(scale) {
+  current_project.zoom = scale;
+}
+function getScale() {
+  return current_project.zoom;
+}
+window.addEventListener("keydown", (e) => {
+  if(e.key == "Shift") {
+    SHIFT = true;
+  }
+});
+window.addEventListener("keyup", (e) => {
+  if(e.key == "Shift") {
+    SHIFT = false;
+  }
+});
 window.addEventListener("mousemove", (e) => {
   M_X = e.clientX;
   M_Y = e.clientY;
   M_DELTA_X = M_X - M_INIT_X;
   M_DELTA_Y = M_Y - M_INIT_Y;
+  if (EDIT_MODE === "MOVE" && M_BUTTON_LEFT) {
+    setPositionDraw(
+      current_project.position.x + M_DELTA_X,
+      current_project.position.y + M_DELTA_Y
+    );
+  }
 });
 window.addEventListener("wheel", (e) => {
   if (e.deltaY < 0) {
@@ -29,6 +64,52 @@ window.addEventListener("wheel", (e) => {
   isScrolling = setTimeout(() => {
     SCROLL_STATE = "STOP"
   }, 300);
+  switch (EDIT_MODE) {
+    case "SELECTION":
+      const space = 20;
+      switch (SCROLL_STATE) {
+        case "UP":
+          if (SHIFT) {
+            setPositionDraw(current_project.position.x + space,current_project.position.y);
+            setPositionProject(current_project.position.x + space,current_project.position.y);
+          }
+          else {
+            setPositionDraw(current_project.position.x,current_project.position.y + space);
+            setPositionProject(current_project.position.x,current_project.position.y + space);
+          }
+          break;
+        case "DOWN":
+          if (SHIFT) {
+            setPositionDraw(current_project.position.x - space,current_project.position.y);
+            setPositionProject(current_project.position.x - space,current_project.position.y);
+          }
+          else {
+            setPositionDraw(current_project.position.x,current_project.position.y - space);
+            setPositionProject(current_project.position.x,current_project.position.y - space);
+          }
+          break;
+        default:
+          break;
+      }
+      break;
+    case "ZOOM":
+      const scale_space = 0.1;
+      switch (SCROLL_STATE) {
+        case "UP":
+          setScaleDraw(current_project.zoom + scale_space)
+          setScaleProject(current_project.zoom + scale_space)
+          break;
+        case "DOWN":
+          setScaleDraw(current_project.zoom - scale_space)
+          setScaleProject(current_project.zoom - scale_space)
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
 });
 window.addEventListener("mouseup", (e) => {
   if (e.button === 0) {
@@ -40,15 +121,21 @@ window.addEventListener("mouseup", (e) => {
   else if (e.button === 2) {
     M_BUTTON_RIGHT = false;
   }
-  if (EDIT_MODE === "MOVE") {
-    current_project.position.x += M_DELTA_X;
-    current_project.position.y += M_DELTA_Y;
+  switch (EDIT_MODE) {
+    case "MOVE":
+      setPositionProject(
+        current_project.position.x += M_DELTA_X,
+        current_project.position.y += M_DELTA_Y
+      )
+      break;
+
+    default:
+      break;
   }
   M_INIT_X = 0;
   M_INIT_Y = 0;
   M_DELTA_Y = 0;
   M_DELTA_Y = 0;
-  console.log("here")
 });
 window.addEventListener("mousedown", (e) => {
   if (e.button === 0) {
@@ -62,11 +149,4 @@ window.addEventListener("mousedown", (e) => {
   }
   M_INIT_X = e.clientX;
   M_INIT_Y = e.clientY;
-});
-
-window.addEventListener("mousemove", (e) => {
-  if (EDIT_MODE === "MOVE" && M_BUTTON_LEFT) {
-    const draw = document.getElementById("project_draw");
-    draw.style.transform = `translate(${current_project.position.x + M_DELTA_X}px, ${current_project.position.y + M_DELTA_Y}px)`;
-  }
 });
